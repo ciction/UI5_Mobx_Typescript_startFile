e283eb7196c8b34b382ff341eef3b400d687baf6
Christophe Swolfs <christophe.swolfs@infrabel.be>
2025-12-05 13:42:28 +0000

    working Mobx Typescript setup

--- webapp/controller/View1.controller.ts
+++ webapp/controller/View1.controller.ts
@@ -1,12 +1,18 @@
 import Controller from "sap/ui/core/mvc/Controller";
+import { mainStore } from "../store/MainStore";
+import { MobxModel } from "cpro/js/ui5/mobx/MobxModel";
 
+
+/**
 /**
  * @namespace be.cds.mobxts.controller
  */
 export default class View1 extends Controller {
+  public onInit(): void {
+        // 1. Create the MobxModel wrapping our store
+        const model = new MobxModel(mainStore);
 
-    /*eslint-disable @typescript-eslint/no-empty-function*/
-    public onInit(): void {
-
+        // 2. Bind it to the view (unnamed model = default)
+        this.getView()?.setModel(model);
     }
 }
